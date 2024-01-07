664970fa2beb598d5689680ef11a05a3c32bf4e3
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2024-01-07 09:45:56 -0600

    Fix Project Deps

--- fabric/src/main/java/com/unrealdinnerbone/jamd/data/AdvancementProvider.java
+++ fabric/src/main/java/com/unrealdinnerbone/jamd/data/AdvancementProvider.java
@@ -30,7 +30,7 @@ public class AdvancementProvider extends FabricAdvancementProvider {
                                 AdvancementType.TASK,
                                 true,
                                 true,
-                                false
+                                true
                         )
                         .addCriterion("enter_dimension", ChangeDimensionTrigger.TriggerInstance.changedDimensionTo(JAMDRegistry.OVERWORLD.getKey().level()))
                 .build(new ResourceLocation(JAMD.MOD_ID, "enter_mining_dimension")));
